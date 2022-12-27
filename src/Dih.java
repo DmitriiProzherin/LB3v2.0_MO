dcb43b02c4a3b5b39680c66626bcafc8ac2d944c
BehemothDi <dmitriiprozherin@mail.ru>
2022-12-27 11:00:39 +0300

    -

--- src/Dih.java
+++ src/Dih.java
@@ -1,34 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Dih {
 
+    ArrayList<double[]> list = new ArrayList<>();
+
     public double f(double x){
-        return Math.pow(x, 2);
+        return Math.pow(x, 32);
     }
 
-    public void search(double a, double b, double bias, double eps) {
+    public void search(double a, double b, double eps,  double delta) {
 
-        while (true) {
-            double length = b - a;
-            double mid = a + length / 2.0;
+        double length = b - a;
+        double mid = a + length / 2.0;
 
-            double left = mid - bias;
-            double right = mid + bias;
-
-            double f_left = f(left);
-            double f_right = f(right);
-            double f_mid = f(mid);
-
-            System.out.printf("%10f%10f%10f%10f%10f%10f%10f%10f\n", length, a, b, left, right, f_left, f_mid, f_right);
-
-            if (length < 2*eps) {
-                System.out.printf("%10f %10f\n", mid, f(mid));
-                System.exit(-101);
-            }
-
-            if (f_left < f_right) {
-                b = right;
-            }
-            else a = left;
+        if (delta >= eps) {
+            System.out.println("Error");
+            System.exit(13);
         }
+
+        if (length <= 2 * eps) {
+
+            list.forEach(ar -> {
+
+                System.out.printf("%3d%10f%10f%10f%10f%10f%10f%10f%10f\n",
+                        list.indexOf(ar), ar[0], ar[1], ar[2], ar[3], ar[4], ar[5], ar[6], ar[7]);
+
+            });
+
+            System.out.printf("%10f%10f%10f", a, b, mid);
+            System.exit(0);
+        }
+
+        double left = mid - delta / 2;
+        double right = mid + delta / 2;
+
+        double f_left = f(left);
+        double f_right = f(right);
+
+        list.add(new double[] {length, a, b, left, mid, right, f_left, f_right});
+
+        if (f_left < f_right) search(a, right, eps, delta);
+        else search(left, b, eps, delta);
+
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         Dih dih = new Dih();
-        dih.search(-1, 1, 0.01, 0.05);
+        System.out.printf("%3s%10s%10s%10s%10s%10s%10s%10s%10s\n",
+                "k","delta_k","a","b", "m-d","m","m+d",
+                "f(m-d)","f(m+d)");
+
+        dih.search(0, 1, 0.2, 0.1);
     }
 }
